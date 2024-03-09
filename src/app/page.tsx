850c5174493d166083aac46229c4d8c59e3f25d2
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-03-09 10:46:35 +0100

    feat(component): input component creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Button from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import Image from 'next/image'
 
 export default function Home() {
-  return <Button>sign-in</Button>
+  return (
+    <div>
+      <Button>sign-in</Button>
+      <Input placeholder="enter text here" />
+    </div>
+  )
 }
